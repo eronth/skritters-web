ff08a5d3b407f5efe79c4b56821ea0b655f3bfd9
Nic Pereira <github@nicpereira.dev>
2025-04-15 21:49:24 -0500

    Creating tag objects.

---
+++ src/common/Tags/Tag.tsx
@@ -0,0 +1,16 @@
+import { Keyword } from "../../types/keywords";
+
+type Props = {
+  tag: Keyword;
+  className?: string;
+  style?: React.CSSProperties;
+};
+
+
+export default function Tag({ tag, className, style }: Props) {
+  return (
+    <span className={`tag ${className || ''}`} style={style}>
+      {tag}
+    </span>
+  );
+}

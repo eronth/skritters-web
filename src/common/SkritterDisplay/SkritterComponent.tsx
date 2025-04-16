138e439183cbba6226b756999f102b0c4ff0fb20
Nic Pereira <github@nicpereira.dev>
2025-04-15 23:50:14 -0500

    Update individual skritter display component to be way nicer.

---
+++ src/common/SkritterDisplay/SkritterComponent.tsx
@@ -0,0 +1,50 @@
+import { Skritter } from "../../types/types";
+import CreatureTags from "../Tags/SkritterTags";
+import './SkritterComponent.css';
+import StatsTable from "./StatsTable";
+
+type Props = {
+  skritter: Skritter;
+};
+
+export default function SkritterComponent({ skritter }: Props) {
+  
+
+  return (
+    <div className="skritter-card">
+      <div className="skritter-header">
+        <h2 className="skritter-title">{skritter.name}</h2>
+        <CreatureTags tags={skritter.tags} />
+      </div>
+      
+      <p className="skritter-description">{skritter.description}</p>
+      
+      <StatsTable stats={skritter.stats} />
+      
+      {/* Abilities section */}
+      <div>
+        <h3 className="section-title">Abilities</h3>
+        <div className="abilities-container">
+          {skritter.abilities.map((ability, idx) => (
+            <div key={idx} className="ability">
+              <h4 className="ability-name">{ability.name}</h4>
+              <div className="ability-effect">{ability.effect}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+      
+      {/* Retirement section */}
+      {skritter.retirement.length > 0 && (
+        <div>
+          <h3 className="section-title">Retirement</h3>
+          <div className="retirement-container">
+            {skritter.retirement.map((effect, id) => (
+              <div key={id} className="retirement-effect">{effect}</div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}

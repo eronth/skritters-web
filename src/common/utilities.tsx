f0243b3df3f631e977ed500ed5d0c99165e491bc
Nic Pereira <github@nicpereira.dev>
2025-05-05 13:12:50 -0500

    util for calculating average successes for a roll.

--- src/common/utilities.tsx
+++ src/common/utilities.tsx
@@ -13,6 +13,38 @@ const formatModifiers = (modifiers: Modifiers): string => {
   return retVal.trim() || '0'; 
 };
 
+const calculateModifiersAverage = (modifiers: Modifiers): number => {
+  if (modifiers.nilout) return 0;
+
+  let total = 0;
+  
+  // Calculate average from dice
+  if (modifiers.dice) {
+    for (const die of modifiers.dice) {
+      total += die.count * ((die.sides + 1) / 2);
+    }
+  }
+
+  // Add size average bonus (1 per size change)
+  if (modifiers.size) {
+    total += modifiers.size.reduce((sum, val) => sum + val, 0);
+  }
+
+  // Calc average successes so far
+  // DO THIS BEFORE ADDING SUCCESSES IN.
+  total /= 3;
+
+  // Now add successes
+  if (modifiers.successes) {
+    total += modifiers.successes.reduce((sum, val) => sum + val, 0);
+  }
+
+  // truncate to 3 decimal places
+  total = Math.round(total * 1000) / 1000;
+  return total;
+}
+
 export {
-  formatModifiers
+  formatModifiers,
+  calculateModifiersAverage
 };

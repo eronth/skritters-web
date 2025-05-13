1c086c2bb3de8e81402566a1848b4b03715e0fff
Nic Pereira <github@nicpereira.dev>
2025-05-13 00:36:45 -0500

    Fix

--- src/common/Keywords/rules/roll-modifiers/MinusDice.tsx
+++ src/common/Keywords/rules/roll-modifiers/MinusDice.tsx
@@ -2,10 +2,10 @@ import Keyword from "../../Keyword";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDiceD6 } from "@fortawesome/free-solid-svg-icons";
 
-export default function PlusDice({ x }: { x: number }) {
+export default function MinusDice({ x }: { x: number }) {
   
   return (
-    <Keyword className="plus-dice" >
+    <Keyword className="minus-dice" >
       {`-${x}`}<FontAwesomeIcon icon={faDiceD6} />
     </Keyword>
   );

91dac101e45472407c691b34dc82c11e2c7b5c4e
Nic Pereira <github@nicpereira.dev>
2026-04-03 21:41:19 -0500

    Create defeat and retreat section.

--- src/pages/HowToPlay/ActionsRulesComponent.tsx
+++ src/pages/HowToPlay/ActionsRulesComponent.tsx
@@ -1,3 +1,4 @@
+import DefeatAndRetreat from './DefeatAndRetreat';
 import './ActionRulesComponent.css';
 
 export default function ActionsRulesComponent() {
@@ -9,8 +10,9 @@ export default function ActionsRulesComponent() {
     <h3>Movement Actions</h3>
     <h4>Basic Movement Rules</h4>
     <p>
-      The Measurement String — When moving a model, use a measurement string 
-      of the right length. Place one end touching the base of the moving model, 
+      The Measurement String — When moving a model, use a
+      measurement string of the right length. Place one end
+      touching the base of the moving model, 
       then, weaving up down and around terrain, place the string 
       on the battlefield marking out the path you wish to take.
       The model may end up in any location they fit where their
@@ -19,9 +21,20 @@ export default function ActionsRulesComponent() {
     </p>
     <h4>Movement Modifiers</h4>
     <p>
-      These modifiers can be added to any movement attempt, though some may leave very little room for success. Plot the entire route you intend to make before rolling any checks that are called for. If checks do not result in a success, or result in less successes than required, perform your movement until the point of failure, then resolve. If you have multiple Climb modifiers in a single move, you only roll a singular Climb check and distribute the successes as needed. The same is true for other modifiers.
+      These modifiers can be added to any movement attempt,
+      though some may leave very little room for success. Plot
+      the entire route you intend to make before rolling any
+      checks that are called for. If checks do not result in
+      a success, or result in less successes than required,
+      perform your movement until the point of failure, then 
+      resolve. If you have multiple Climb modifiers in a single
+      move, you only roll a singular Climb check and distribute
+      the successes as needed. The same is true for other modifiers.
       <br /><br />
-      <i>Climbing</i> — Roll a SuCheck(3d6) climb check. You may climb 1” per success. If you are climbing a difficult surface, you must successfully climb all the way to the top in one check or you slide back down to the bottom.
+      <i>Climbing</i> — Roll a SuCheck(3d6) climb check. You may
+      climb 1” per success. If you are climbing a difficult
+      surface, you must successfully climb all the way to the
+      top in one check or you slide back down to the bottom.
       <br /><br />
       <i>Jump</i> — ??? – Same as climb but 2d6, decided during gameplay. 
       <br /><br />
@@ -49,7 +62,7 @@ export default function ActionsRulesComponent() {
     <p>
       Make a SuCheck(2d6) and move your movement + successes in a straight line. If you end up in 1”, you’re in a Scuffle! (no longer needed? Updates needed?) Choose an enemy Skritter as a target, then make a 2d6 Charge Check. You may move your full movement stat plus successes from your Charge in a straight line towards the target, and may not make a partial movement. If you end up within your Brawl Threat range, you may close the gap by moving directly towards the target.
       The target of your charge cannot Intercept your charge (unless you fail to get into base2base???).
-      (maybe doing a charge gives -DICE to target’s defends based on successes or something????)
+      (maybe doing a charge gives -DICE to target's defends based on successes or something????)
     </p>
     <h3>Swapping Weapons Action</h3>
     <p>
@@ -57,7 +70,7 @@ export default function ActionsRulesComponent() {
     </p>
     <h3>Attack Action</h3>
     <p>
-      Roll the dice for the attack you’re making, adding in any relevant bonuses from weapon or equipment. Determine successes (total result divided by 3, discard remainder), each success is a hit! Your target gets to roll defense. Each successful defense blocks a hit, and any unblocked hits deal 1 damage each.
+      Roll the dice for the attack you're making, adding in any relevant bonuses from weapon or equipment. Determine successes (total result divided by 3, discard remainder), each success is a hit! Your target gets to roll defense. Each successful defense blocks a hit, and any unblocked hits deal 1 damage each.
       Brawl attacks can only be used in a Scuffle. You can attempt to Close the Gap immediately before making a Brawl Attack as a free action. (unarmed attack: 0.5”, -4 SIZE)
       Ranged attacks have an effective range. Attacking beyond that range confers -1DICE and -1SIZE for every range beyond the effective range. Ranged attacks can not be used in a Scuffle. (MAYBE THEY ONLY GET -1DICE IN A SCUFFLE).
       Weave attacks cannot be used beyond their max range. However, they often have additional and unusual effects on hits, sometimes replacing damage with some other effect.
@@ -67,5 +80,6 @@ export default function ActionsRulesComponent() {
     <p>
       If a unit can only see half or less of another unit when making an attack, the targeted unit is considered to have cover. Cover grants -1 SIZE to the attack, and +1 SIZE to the unit’s defence check.
     </p>
+    <DefeatAndRetreat />
   </>);
 }
---
+++ src/pages/HowToPlay/DefeatAndRetreat.tsx
@@ -0,0 +1,21 @@
+export default function DefeatAndRetreat() {
+  return (
+    <>
+      <h2>Defeat and Retreat</h2>
+      <p>
+        If a Skritter loses all of their health,
+        they are too injured and become defeated,
+        forcing them to immediately retreat. A
+        Skritter that retreats is removed from the
+        battlefield and takes no more activations.
+        After a battle, you must make a recovery check
+        for each Skritter that retreated. If a Skritter
+        fails their recovery check, they must retire.
+        However, a retired Skritter is not gone, for they
+        still have much wisdom to share. Consult your
+        Skritter’s *retired* section to see what benefits
+        they still have to offer.
+      </p>
+    </>
+  );
+}

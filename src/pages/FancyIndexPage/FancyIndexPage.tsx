899bc6668243b2b98fb905404ee50e01a1f92cfa
Nic Pereira <github@nicpereira.dev>
2026-04-08 17:10:53 -0500

    move

---
+++ src/pages/FancyIndexPage/FancyIndexPage.tsx
@@ -0,0 +1,20 @@
+import GameTitle from "../../common/GameTitle";
+import NavCluster from "../../common/NavCluster";
+import './FancyIndexPage.css';
+
+export default function FancyIndexPage() {
+  return (<div className='fancy-index'>
+    <GameTitle isIndex={true} />
+    <div>
+      <div className='left-side'>
+        <NavCluster />
+      </div>
+      <div className='right-side'>
+        <p>
+          Short description or introduction about the Skritters game.
+          Short description or introduction about the Skritters game.
+        </p>
+      </div>
+    </div>
+  </div>);
+}

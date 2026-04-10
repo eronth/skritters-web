b9c344f7d8215e0831577cc86276b9027605c801
Nic Pereira <github@nicpereira.dev>
2026-04-10 13:27:54 -0500

    scenario page intro

--- src/pages/Scenarios/ScenariosPage.tsx
+++ src/pages/Scenarios/ScenariosPage.tsx
@@ -1,12 +1,75 @@
+import Bsp from "../../common/Formatting/bsp";
 import Page from "../Page";
 import scenarios from "../../data/scenario-data";
 import './ScenariosPage.css';
 
 export default function ScenariosPage() {
+  const indexUnify = {
+    joinTo: 5,
+    joinOf: 6
+  };
+
+  const rollableTable = <>
+    {scenarios.map((scenario, index) => {
+      if (indexUnify.joinOf == index) { return null; }
+
+      let nameLink = (
+        <a href={'#scenario-' + index}>
+          {scenario.name}    
+          </a>
+      );
+      if (indexUnify.joinTo == index) {
+        const joinedNameLink = (
+          <a href={'#scenario-' + indexUnify.joinOf}>
+            {scenarios[indexUnify.joinOf].name}
+          </a>
+        );
+        nameLink = <>{nameLink} / {joinedNameLink}</>
+      }
+
+      const tableCount = (
+        (index > indexUnify.joinTo)
+        ? index-1
+        : index
+      );
+
+      return (
+        <div key={index} className="roll-table-row">
+          <div className="number">
+            {tableCount+1}
+          </div>
+          <div>
+            —<Bsp />{nameLink}
+          </div>
+        </div>
+      )
+    })}
+  </>;
+
   return (<Page tab="scenarios">
+    <p>
+      Below is the list of possible scenarios you can
+      play in a game of Skritters. You can select a
+      scenario by rolling a dice, or by choosing one
+      that interests you.
+    </p>
+    <p>
+      Each scenario provides a unique set of challenges
+      and objectives for players to navigate. Scenarios
+      are broken into sections for easier understanding.
+    </p>
+    <p>
+      Setup — This details any additional setup needed
+    </p>
+    <div>
+      // TODO: DEPLOYMENT ZONES
+    </div>
+    <div className="roll-table">
+      {rollableTable}
+    </div>
     {scenarios.map((scenario, index) => (
       <div key={index} className="scenario">
-        <h1>{scenario.name}</h1>
+        <h1 id={'scenario-' + index}>{scenario.name}</h1>
         <h2>Setup</h2>
         <p>{scenario.setup}</p>
         <h2>Deployment</h2>
